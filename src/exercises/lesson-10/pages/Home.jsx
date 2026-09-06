5ce0127d5a2fe607944039156baa05dc31cd6722
jacquelineb <jacquelinesotheremail@gmail.com>
2026-09-05 22:04:44 -0700

    Finish exercise 3: Dynamic routes

--- src/exercises/lesson-10/pages/Home.jsx
+++ src/exercises/lesson-10/pages/Home.jsx
@@ -36,7 +36,9 @@ export default function Home({ products }) {
               }}
             />
 
-            <h3 style={{ margin: '10px 0 4px' }}>{p.name}</h3>
+            <Link to={`/lessons/lesson-10/products/${p.id}`}>
+              <h3 style={{ margin: '10px 0 4px' }}>{p.name}</h3>
+            </Link>
 
             <p style={{ margin: 0 }}>
               <strong>${p.price.toFixed(2)}</strong>
