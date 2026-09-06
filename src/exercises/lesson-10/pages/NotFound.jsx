474b533537f7b5901906486a4033cf35ba962a44
jacquelineb <jacquelinesotheremail@gmail.com>
2026-09-05 22:16:08 -0700

    Finish exercise 4: useNavigate + useLocation + 404 Support

--- src/exercises/lesson-10/pages/NotFound.jsx
+++ src/exercises/lesson-10/pages/NotFound.jsx
@@ -1,11 +1,15 @@
 import { Link, useLocation } from 'react-router-dom';
 
 export default function NotFound() {
-  const pathname = '';
+  const location = useLocation();
+  const pathname = location.pathname;
+  console.log(pathname);
 
   return (
     <section>
       <h2>404: Not Found</h2>
+      <p>The path {pathname} does not exist</p>
+      <Link to="/lessons/lesson-10/">Go Home</Link>
     </section>
   );
 }
